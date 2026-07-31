003a1fd5cbce31216423a091951edec652e4e384
Lans12345 <olanalans12345@gmail.com>
2026-07-31 08:14:40 +0800

    updated email

--- lib/config/integrations_config.dart
+++ lib/config/integrations_config.dart
@@ -1,8 +1,8 @@
 class IntegrationsConfig {
-  static const String emailJsServiceId = 'service_etivm0m';
-  static const String emailJsTemplateId = 'template_1iyw0ub';
-  static const String emailJsPublicKey = 'eJcxJq9HWCWKMp-5M';
-  static const String emailJsAccessToken = '3w8ighMcLUC7poVnW9-7o';
+  static const String emailJsServiceId = 'service_b5ez4ix';
+  static const String emailJsTemplateId = 'template_17hmw12';
+  static const String emailJsPublicKey = 'LEnABj81nmpvvGCDJ';
+  static const String emailJsAccessToken = 'VC9ew3T8WgVHGrzF_8k0O';
 
   static const String reportSenderEmail = 'admin@citimovers.net';
   static const String reportSenderName = 'CitiMovers';
